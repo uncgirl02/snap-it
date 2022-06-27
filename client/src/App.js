e48e2f2372182a6f662be24c69fc1b6062bd6cca
Erin Ewart <ems1980@hotmail.com>
2022-06-27 19:15:05 -0400

    added Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
@@ -10,42 +7,65 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import {
+  ChakraProvider,
+  theme,
+} from '@chakra-ui/react';
 
 import './App.css';
 
-// import Navbar from './components/Navbar';
-// import Footer from './components/';
 
-// import Home from './pages/Home';
+import Home from './pages/Home';
 // import Dashboard from './pages/Dashboard';
-// import Profile from './pages/Profile';
+// import CreateAlbum from './pages/CreateAlbum';
+// import EditAlbum from './pages/EditAlbum';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
+
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <ChakraProvider>
-
-        </ChakraProvider>
+          <div>
+            <Routes>
+              <ChakraProvider>
+                <Route 
+                  path="/" 
+                  element={<Home />} 
+                />
+                {/* <Route 
+                  path="/dashboard" 
+                  element={<Dashboard />} 
+                />
+                <Route 
+                  path="/create" 
+                  element={<CreateAlbum />} 
+                />
+                <Route 
+                  path="/edit" 
+                  element={<EditAlbum />} 
+                /> */}
+              </ChakraProvider>
+            </Routes>
+          </div>
       </Router>
     </ApolloProvider>
   );
---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Hero from '../components/Hero';
+import Testimonials from '../components/Testimonials';
+
+const Home = () => {
+
+      <main>
+        <div> 
+                <Navbar />
+              <Hero></Hero>
+              <Testimonials></Testimonials>
+        </div>
+      </main>
+  };
+  
+  export default Home;
+  
